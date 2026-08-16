ce0903690f1042ab1fb10d582c67215387e4e11a
swapnachari-marjuk <marjukmujaddedi@gmail.com>
2026-08-16 22:16:29 +0600

    l-1, pr-3

--- test.c
+++ test.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main() {
-    double n;
+int main()
+{
+    int n;
     printf("Enter an integer: ");
     scanf("%d", &n);
+    bool is_prime = true;
 
-    for (double i = 0; i < n; i++)
+    if (n <= 1)
     {
-        // if (n%i!=0)
-        // {
-        //     printf("It's not a prime number.");
-        //     break;
-        // }
-        
+        is_prime = false;
     }
-    
+    else
+    {
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                is_prime = false;
+                break;
+            }
+        }
+    }
+
+    if (is_prime == true)
+    {
+        printf("It's a prime number.\n");
+    }
+    else if (is_prime == false)
+    {
+        printf("It's not a prime number.\n");
+    }
+
     return 0;
 }
